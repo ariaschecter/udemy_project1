415d17fd2eba14591f6aaa9332dedefb26d13bb4
ariaschecter <aria.maulana.m@gmail.com>
2022-12-21 20:30:01 +0700

    [47] Backend Home Page Slider Option Part 3

--- app/Http/Controllers/Home/HomeSliderController.php
+++ app/Http/Controllers/Home/HomeSliderController.php
@@ -5,11 +5,48 @@ namespace App\Http\Controllers\Home;
 use App\Http\Controllers\Controller;
 use App\Models\HomeSlide;
 use Illuminate\Http\Request;
+use Image;
 
 class HomeSliderController extends Controller
 {
     public function HomeSlider() {
         $homeslide = HomeSlide::find(1);
         return view('admin.home_slide.home_slide_all', compact('homeslide'));
+    } // End Method
+
+    public function UpdateSlider(Request $request) {
+        $id = $request->id;
+
+        if ($request->file('home_slide')) {
+            $image = $request->file('home_slide');
+            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+            $save_url = 'upload/home_slide/'.$name_gen;
+            Image::make($image)->resize(636, 852)->save($save_url);
+
+            HomeSlide::findOrFail($id)->update([
+                'title' => $request->title,
+                'short_title' => $request->short_title,
+                'video_url' => $request->video_url,
+                'home_slide' => $save_url,
+            ]);
+
+            $notification = array(
+                'message' => 'Home Slide Updated with Image Successfully',
+                'alert-type' => 'success'
+            );
+            return redirect()->back()->with($notification);
+        } else {
+            HomeSlide::findOrFail($id)->update([
+                'title' => $request->title,
+                'short_title' => $request->short_title,
+                'video_url' => $request->video_url,
+            ]);
+
+            $notification = array(
+                'message' => 'Home Slide Updated without Image Successfully',
+                'alert-type' => 'success'
+            );
+            return redirect()->back()->with($notification);
+        } // End else
     } // End Method
 }
