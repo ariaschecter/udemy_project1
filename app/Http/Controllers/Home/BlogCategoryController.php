519568add35a290be0454170e620535ef2b7f50a
ariaschecter <aria.maulana.m@gmail.com>
2022-12-24 18:48:18 +0700

    [67] Backend Blog Category Setup Part 2

--- app/Http/Controllers/Home/BlogCategoryController.php
+++ app/Http/Controllers/Home/BlogCategoryController.php
@@ -12,4 +12,26 @@ class BlogCategoryController extends Controller
         $blogCategories = BlogCategory::latest()->get();
         return view('admin.blog_category.blog_category_all', compact('blogCategories'));
     } // End Method
+
+    public function AddBlogCategory() {
+        return view('admin.blog_category.blog_category_add');
+    } // End Method
+
+    public function StoreBlogCategory(Request $request) {
+        $request->validate([
+            'blog_category' => 'required|unique:blog_categories,blog_category'
+        ], [
+            'blog_category.required' => 'Blog Category Name is Required',
+        ]);
+
+        BlogCategory::insert([
+            'blog_category' => $request->blog_category,
+        ]);
+        $notification = array(
+            'message' => 'Blog Category Inserted Successfully',
+            'alert-type' => 'success',
+        );
+
+        return redirect()->route('all.blog.category')->with($notification);
+    } // End Method
 }
