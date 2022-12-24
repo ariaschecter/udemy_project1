9ba3f24becacd6e09b74f6fab01d5c3485dadcf2
ariaschecter <aria.maulana.m@gmail.com>
2022-12-24 20:04:12 +0700

    [70] Backend Blog Page Setup Part 2

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Home\HomeSliderController;
 use App\Http\Controllers\Home\AboutController;
 use App\Http\Controllers\Home\BlogCategoryController;
+use App\Http\Controllers\Home\BlogController;
 use App\Http\Controllers\Home\PortfolioController;
 
 /*
@@ -69,6 +70,16 @@ Route::controller(BlogCategoryController::class)->group(function () {
     Route::get('/edit/blog/category/{id}', 'EditBlogCategory')->name('edit.blog.category');
     Route::post('/update/blog/category/{blogcategory}', 'UpdateBlogCategory')->name('update.blog.category');
     Route::get('/delete/blog/category/{blogcategory}', 'DeleteBlogCategory')->name('delete.blog.category');
+});
+
+// Blog All Route
+Route::controller(BlogController::class)->group(function () {
+    Route::get('/all/blog', 'AllBlog')->name('all.blog');
+    // Route::get('/add/blog', 'AddBlog')->name('add.blog');
+    // Route::post('/store/blog', 'StoreBlog')->name('store.blog');
+    // Route::get('/edit/blog/{id}', 'EditBlog')->name('edit.blog');
+    // Route::post('/update/blog/{blog}', 'UpdateBlog')->name('update.blog');
+    // Route::get('/delete/blog/{blog}', 'DeleteBlog')->name('delete.blog');
 });
 
 
